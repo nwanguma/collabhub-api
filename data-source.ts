e48ab1bc88429f089028f9f3c0bf618a26e45f1b
Tochi <nwangumat@hotmail.com>
2025-01-08 08:55:33 +0100

    Cleanup config

--- data-source.ts
+++ data-source.ts
@@ -6,17 +6,12 @@ dotenv.config();
 
 const configService = new ConfigService();
 
-console.log(
-  configService.get<string>('DATABASE_NAME'),
-  configService.get<string>('DATABASE_URL'),
-);
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
   database: configService.get<string>('DATABASE_NAME'),
   url: configService.get<string>('DATABASE_URL'),
   entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
-  synchronize: process.env.NODE_ENV === 'development',
+  synchronize: true,
   cache: {
     duration: 60000,
   },
